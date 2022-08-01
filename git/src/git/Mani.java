de3744f2590eb45845e11191622eebe5ebd60680
Manishfunde <110392957+Manishfunde@users.noreply.github.com>
2022-08-01 17:26:30 +0530

    Update Mani.java

--- git/src/git/Mani.java
+++ git/src/git/Mani.java
@@ -1,13 +1,16 @@
 package git;
 
-public class Mani 
+public class Mani {
 	
 		public void m1()
 		{
+			
 			System.out.println("mi");
 		}
+	public static void main(string args[])
+	{
 		System.out.println("git");
-System.out.println("mani");
+               System.out.println("mani");
 	}
 
 }
