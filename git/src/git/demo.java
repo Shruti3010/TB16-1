3ed6bcafcd9f2d004b52d85908a7e3a585677ded
lenovo <lenovo@192.168.0.4>
2022-08-01 22:41:28 +0530

    new program

---
+++ git/src/git/demo.java
@@ -0,0 +1,24 @@
+package git;
+
+public class demo {
+		
+public void m1()
+{
+	System.out.println("manish");
+	}
+public void M2()
+{
+	System.out.println("shruti");
+}
+public void m3()
+{
+	System.out.println("vaibhav");
+}
+public static void main(String[] args) 
+{
+	demo d=new demo();
+	d.m1();
+	d.M2();
+	d.m3();
+}
+}
