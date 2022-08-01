ed60c3666850f87a165fd1944686ddf92bb6612d
Manishfunde <110392957+Manishfunde@users.noreply.github.com>
2022-08-01 17:17:11 +0530

    by joya team

--- git/src/git/Mani.java
+++ git/src/git/Mani.java
@@ -4,6 +4,10 @@ public class Mani {
 
 	public static void main(String[] args) 
 	{
+		public void m1()
+		{
+			System.out.println("mi");
+		}
 		System.out.println("git");
 System.out.println("mani");
 	}
